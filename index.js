2a89e8fdea8299f96ac29c26c1226e81f0ccdb26
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-05-21 21:16:02 -0300

    requirement 2

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(JSON.parse(content));
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const content = await readFile('talker.json');
+  const { id } = req.params;
+  const talkerById = JSON.parse(content).find((talker) => talker.id === parseInt(id, 10));
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(talkerById);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
